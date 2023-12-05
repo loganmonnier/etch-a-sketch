f6a741dcc40128d643c942b4243edb2029ede284
Logan Monnier <lfmonnier@gmail.com>
2023-12-04 19:17:48 -0500

    define size of container div, add flex to grid-section

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
-const container = querySelector('container');
+const container = document.querySelector('.container');
 
 let numRows = 16
 let numCols = 16
 
-for (let r = 0; r < 16; r++){
-    for (let c = 0; c < 16; c++){
+for (let r = 0; r < numRows; r++){
+    for (let c = 0; c < numCols; c++){
         const gridSquare = document.createElement('div');
         gridSquare.classList.add('grid-section')
         container.appendChild(gridSquare);
