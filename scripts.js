9543e48c5de48d7178472911464583e14d6a711a
Logan Monnier <lfmonnier@gmail.com>
2023-12-05 10:09:42 -0500

    add event listener to highlight grid sections

--- scripts.js
+++ scripts.js
@@ -1,12 +1,21 @@
 const container = document.querySelector('.container');
 
-let numRows = 16
-let numCols = 16
+let numRows = 16;
+let numCols = 16;
 
 for (let r = 0; r < numRows; r++){
     for (let c = 0; c < numCols; c++){
         const gridSquare = document.createElement('div');
-        gridSquare.classList.add('grid-section')
+        gridSquare.classList.add('grid-square')
         container.appendChild(gridSquare);
-    }
-}
+
+        gridSquare.addEventListener('mouseenter', function(){
+            this.style.backgroundColor = 'black';
+        })
+        
+    } 
+};
+
+
+
+
