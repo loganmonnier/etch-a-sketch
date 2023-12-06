73ef85dfccf50c26edb13c59be0856de3e73aa00
Logan Monnier <lfmonnier@gmail.com>
2023-12-06 11:12:54 -0500

    fix aspect ratio of grid squares

--- scripts.js
+++ scripts.js
@@ -3,8 +3,10 @@ const input = document.querySelector("input");
 const button = document.querySelector("button");
 
 button.addEventListener('click', function(){
-let numRows = parseInt(input);
-let numCols = parseInt(input);
+let numRows = parseInt(input.value);
+let numCols = parseInt(input.value);
+
+container.style.setProperty('--numCols', numCols)
 
 for (let r = 0; r < numRows; r++){
     for (let c = 0; c < numCols; c++){
